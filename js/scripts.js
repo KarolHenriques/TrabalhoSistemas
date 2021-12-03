41885056620e15bd63304a4503a828ecda73efb5
KarolHenriques <karoljares@gmail.com>
2021-12-03 13:07:23 +0000

    Início cálculo para troca de base decinal para binária

--- js/scripts.js
+++ js/scripts.js
@@ -3,28 +3,30 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function convert() {
 
-//Conversão de base decimal para binária
-
-function Convert() {
-
-
-var base1 = document.getElementById("base1").value; 
-var base2 = document.getElementById("base2").value; 
-var n1;
-var sn1;
-var bin;  
-var dec; 
-var hex; 
-var oct; 
-
-if (flexRadioDefaultdec1.checked && flexRadioDefaultdec1.checked){
-    //Conversão decimal para binário:
-    while (n1 > 0) {
-        n1 % 10;
-        n1 = n1 / 10;
-        
+    var base1 = document.getElementById("base1").value; 
+    var i;
+    var bin;  
+    var dec; 
+    var hex; 
+    var oct; 
+    int[100]; bin_num = new int[100];
+    
+    if (flexRadioDefaultdec.checked && flexRadioDefaultbin1.checked){
+        //Conversão decimal para binário:
+        for (i = 0; base1 > 0; ++i){
+            bin_num[i] = base1 % 2;
+            base1 = base1 / 2;
+        }
+        for (i = i - 1; i >= 0; --i){
+        document.getElementById("converter").innerHTML ="Número decimal:" + base1; 
+        document.getElementById("converter").innerHTML ="Número binário:" + bin_num[i];
+        }    
     }
-}
+    
+    
+    
+    
+    
+    }
